8545e5a99b3f4d3b6e29bb98dd615e3331fff6f5
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-09-04 10:47:19 -0700

    scaffold contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,52 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: %i[show edit update destroy]
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to @contact, notice: "Contact was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to @contact, notice: "Contact was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    redirect_to contacts_url, notice: "Contact was successfully destroyed."
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def contact_params
+    params.require(:contact).permit(:name, :email, :twitter, :phone)
+  end
+end
---
+++ spec/factories/contacts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :contact do
+    name { "MyString" }
+    email { "MyString" }
+    twitter { "MyString" }
+    phone { "MyString" }
+  end
+end
